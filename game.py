44165534e2e3d23cc3f4d96c85dfd5686022e546
Filip Tichý <filiptichy888@gmail.com>
2023-06-18 10:25:35 +0200

    idk whats going on

--- game.py
+++
@@ -1,6 +0,0 @@
-for i in range(int(input("pass a number "))):
-    y = 0
-    print(y + int(i) + 1)
-
-print("fault")
-print("bruh")
---
+++ intro.py
@@ -0,0 +1,4 @@
+HEIGHT = 600
+WIDTH = 650
+def draw():
+    screen.fill((0, 0, 0))
