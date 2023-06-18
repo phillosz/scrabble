90dec39122982db6ab4857bab40b2ab19baa7217
Filip Tichý <filiptichy888@gmail.com>
2023-06-18 11:09:49 +0200

    bruh

--- intro.py
+++ intro.py
@@ -1,4 +1,20 @@
-HEIGHT = 500
-WIDTH = 550
+alien = Actor("alien")
+alien.pos = 200, 60
+WIDTH = 500
+HEIGHT = alien.height + 60
+
 def draw():
-    screen.fill((0, 0, 0))
+    screen.clear()
+    alien.draw()
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        print("Hit")
+    else:
+        print("Miss")
