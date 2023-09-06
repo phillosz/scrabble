adc6e77e085c6274fc7549f87f482203b75e2648
Filip Tichý <filiptichy888@gmail.com>
2023-09-06 19:52:22 +0200

    coords write single letters not words

--- intro.py
+++ intro.py
@@ -20,32 +20,88 @@ arr = [[4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4, 5,],
     [4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4, 5]]
 
 
-for i in arr:
-    for each in i:
-        if each == 4:
-            print("TW ", end=" ")
-        elif each == 3:
-            print("DW ", end=" ")
-        elif each == 2:
-            print("TL ", end=" ")
-        elif each == 1:
-            print("DL ", end=" ")
-        elif each == 0:
-            print("___", end=" ")
-        elif each == 5:
-            print("\n\n", end=" ")
-        elif each == 6:
-            print(" S ", end=" ")
-        elif each == 7:
-            print("__", end=" ")
-    print()
+def main_func():
+    for i in arr:
+        for each in i:
+            if each == 4:
+                print("TW ", end=" ")
+            elif each == 3:
+                print("DW ", end=" ")
+            elif each == 2:
+                print("TL ", end=" ")
+            elif each == 1:
+                print("DL ", end=" ")
+            elif each == 0:
+                print("___", end=" ")
+            elif each == 5:
+                print("\n\n", end=" ")
+            elif each == 6:
+                print(" S ", end=" ")
+            elif each == 7:
+                print("__", end=" ")
+            elif each == "A":
+                print(" A ", end=" ")
+            elif each == "B":
+                print(" B ", end=" ")
+            elif each == "C":
+                print(" C ", end=" ")
+            elif each == "D":
+                print(" D ", end=" ")
+            elif each == "E":
+                print(" E ", end=" ")
+            elif each == "F":
+                print(" F ", end=" ")
+            elif each == "G":
+                print(" G ", end=" ")
+            elif each == "H":
+                print(" H ", end=" ")
+            elif each == "CH":
+                print("CH ", end=" ")
+            elif each == "I":
+                print(" I ", end=" ")
+            elif each == "J":
+                print(" J ", end=" ")
+            elif each == "K":
+                print(" K ", end=" ")
+            elif each == "L":
+                print(" L ", end=" ")
+            elif each == "M":
+                print(" M ", end=" ")
+            elif each == "N":
+                print(" N ", end=" ")
+            elif each == "O":
+                print(" O ", end=" ")
+            elif each == "P":
+                print(" P ", end=" ")
+            elif each == "Q":
+                print(" Q ", end=" ")
+            elif each == "R":
+                print(" R ", end=" ")
+            elif each == "S":
+                print(" S ", end=" ")
+            elif each == "T":
+                print(" T ", end=" ")
+            elif each == "U":
+                print(" U ", end=" ")
+            elif each == "V":
+                print(" V ", end=" ")
+            elif each == "X":
+                print(" X ", end=" ")
+            elif each == "Y":
+                print(" Y ", end=" ")
+            elif each == "Z":
+                print(" Z ", end=" ")
+        print()
             
+main_func()
             
 x = input("Enter X coords of the start ")
 y = input("Enter Y coords of the start ")
+word = []
+word.append(input("Enter your word in CAPITALS "))
 
 target = arr[int(x)][int(y)]
-print(target)
+arr[int(x)][int(y)] = word[0]
 
-
+main_func()
 #input("Enter your word in CAPITALS ")
